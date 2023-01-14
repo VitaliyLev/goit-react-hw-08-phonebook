886335ddc2bde7691577b5cdc53b39c8a5c11e91
VitaliyLev <111646762+VitaliyLev@users.noreply.github.com>
2023-01-14 14:24:42 +0200

    add registr and login form styles

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,33 @@
 import { Typography } from '@mui/material';
+// import Box from '@mui/material/Box';
+// import TextField from '@mui/material/TextField';
+// import { useDispatch } from 'react-redux';
+// import { register } from 'redux/auth/operations';
+// import Button from '@mui/material/Button';
 
 export default function Home() {
+  // const dispatch = useDispatch();
+
+  // const handleSubmit = e => {
+  //   e.preventDefault();
+  //   const form = e.currentTarget;
+  //   const asdsad = {
+  //     name: form.elements.name.value,
+  //     email: form.elements.email.value,
+  //     password: form.elements.password.value,
+  //   };
+  //   console.log(asdsad);
+
+  //   // dispatch(
+  //   //   register({
+  //   //     name: form.elements.name.value,
+  //   //     email: form.elements.email.value,
+  //   //     password: form.elements.password.value,
+  //   //   })
+  //   // );
+  //   // form.reset();
+  // };
+
   return (
     <>
       <Typography variant="h5" sx={{ color: 'text.primary' }}>
@@ -10,6 +37,73 @@ export default function Home() {
       <Typography sx={{ color: 'text.primary' }}>
         Cтилі не були обовязковими, їх допишу обовязково
       </Typography>
+      {/* 
+      <Box
+        onSubmit={handleSubmit}
+        component="form"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          // '& .MuiTextField-root': { width: '50ch' },
+          // ml: 'auto',
+          // mr: 'auto',
+          // width: 500,
+          // maxWidth: '100%',
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          name="name"
+          label="Name"
+          type="text"
+          autoComplete="off"
+          sx={{
+            display: 'block',
+            mb: '15px',
+            ml: 'auto',
+            mr: 'auto',
+          }}
+        />
+        <TextField
+          name="email"
+          label="Email"
+          type="email"
+          autoComplete="off"
+          sx={{
+            display: 'block',
+            mb: '15px',
+            ml: 'auto',
+            mr: 'auto',
+          }}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          sx={{
+            display: 'block',
+            mb: '15px',
+            ml: 'auto',
+            mr: 'auto',
+          }}
+        />
+
+        <Button
+          type="submit"
+          variant="outlined"
+          sx={{
+            // maxWidth: '90px',
+            ml: 'auto',
+            mr: 'auto',
+            display: 'block',
+          }}
+        >
+          Register
+        </Button>
+
+      </Box> */}
     </>
   );
 }
--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,6 +1,12 @@
 import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/auth/operations';
-import css from './LoginForm.module.css';
+// import css from './LoginForm.module.css';
+
+import { Typography } from '@mui/material';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+import Button from '@mui/material/Button';
 
 export default function LoginForm() {
   const dispatch = useDispatch();
@@ -20,18 +26,82 @@ export default function LoginForm() {
 
   return (
     <>
-      <h1>Залогіньтесь було б круто!</h1>
+      {/* <h1>Залогіньтесь було б круто!</h1>
       <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
         <label className={css.label}>
           Email
-          <input type="email" name="email" />
+          <input type="email" name="email" autoComplete="off" />
         </label>
         <label className={css.label}>
           Password
-          <input type="password" name="password" />
+          <input type="password" name="password" autoComplete="off" />
         </label>
         <button type="submit">Log In</button>
-      </form>
+      </form> */}
+
+      <Typography
+        variant="h5"
+        sx={{
+          color: 'text.primary',
+          mb: '15px',
+        }}
+      >
+        Залогіньтесь було б круто!
+      </Typography>
+      <Box
+        onSubmit={handleSubmit}
+        component="form"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          // '& .MuiTextField-root': { width: '50ch' },
+          // ml: 'auto',
+          // mr: 'auto',
+          // width: 500,
+          // maxWidth: '100%',
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField
+          name="email"
+          label="Email"
+          type="email"
+          autoComplete="off"
+          sx={{
+            display: 'block',
+            mb: '15px',
+            ml: 'auto',
+            mr: 'auto',
+          }}
+        />
+        <TextField
+          name="password"
+          label="Password"
+          type="password"
+          autoComplete="current-password"
+          sx={{
+            display: 'block',
+            mb: '15px',
+            ml: 'auto',
+            mr: 'auto',
+          }}
+        />
+        {/* <button> */}
+        <Button
+          type="submit"
+          variant="outlined"
+          sx={{
+            // maxWidth: '90px',
+            ml: 'auto',
+            mr: 'auto',
+            display: 'block',
+          }}
+        >
+          Register
+        </Button>
+        {/* </button>11 */}
+      </Box>
     </>
   );
 }
